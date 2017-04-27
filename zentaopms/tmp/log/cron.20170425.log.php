81a2b3ac90f0e58529bfa228637855300004dfa8
UnLike-jie <761523412@qq.com>
2017-04-27 21:24:47 +0800

    task code

---
+++ zentaopms/tmp/log/cron.20170425.log.php
@@ -0,0 +1,57 @@
+<?php
+ die();
+14:25:08 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+14:30:08 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+14:35:08 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+14:40:08 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+14:45:08 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+14:50:08 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+14:55:08 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+15:00:08 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+15:05:08 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+15:10:08 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+15:15:08 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
